e993af4c9bdf517752ffd256afe12eac7b914b55
Yves OBAME <yviwilli@yahoo.fr>
2018-08-19 23:59:50 +0200

    Test de lecture d'un json

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace smallTalkManager
 {
@@ -11,7 +13,16 @@ namespace smallTalkManager
     {
         static void Main(string[] args)
         {
+            //    JObject o1 = JObject.Parse(File.ReadAllText(@"C:\Users\yviwi\source\repos\ConsoleApp1\ConsoleApp1\usersays\smalltalk.agent.age_usersays_fr.json"));
 
+            //    // read JSON directly from a file
+            //    using (StreamReader file = File.OpenText(@"C:\Users\yviwi\source\repos\ConsoleApp1\ConsoleApp1\usersays\smalltalk.agent.age_usersays_fr.json"))
+            //    using (JsonTextReader reader = new JsonTextReader(file))
+            //    {
+            //        JObject o2 = (JObject)JToken.ReadFrom(reader);
+            //        Console.WriteLine(o2[0]["id"]);
+            //    }
+            Console.WriteLine("haha");
         }
     }
 }
